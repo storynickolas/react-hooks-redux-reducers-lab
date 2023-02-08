6942cccac35472fdee3a3a823ae5f265d8fd7f1c
Nick Story <storynickolasl@gmail.com>
2023-02-08 11:43:38 -0500

    commit tree 37a4f3e08d401058e64f0cda183b9ba07e3eca4f

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,21 @@
 export function manageFriends(state, action) {
   // your code here
+  let newState = {...state}
+  return reducer(newState, action)
+}
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "friends/add":
+      let newFriend = state.friends
+      newFriend.push(action.payload)
+      return { ...state,  friends: newFriend};
+    case "friends/remove":
+      let newFriends = state.friends
+      newFriends = newFriends.filter(friend => friend.id !== action.payload)
+      return { ...state,  friends: newFriends};
+    default:
+      return state;
+  }
 }
